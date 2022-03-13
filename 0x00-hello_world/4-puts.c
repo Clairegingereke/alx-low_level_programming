54dd7842265ae977a92d92ec56b5ace95fc59f38
clairegingereke <clairegingereke@gmail.com>
2022-03-13 01:01:45 +0000

    My fourth task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#iclude <studio.h>
+/**
+ *main -entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main (void)
+{
+puts("/"Programming is like building a multilingaul puzzle");
+return (0);
+} 
